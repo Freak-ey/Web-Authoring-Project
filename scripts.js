50447233b4d3d0d48acb57d603c9b8d74f9a8185
Freak-ey <mkfrary@gmail.com>
2018-11-22 14:51:26 +0000

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -2,53 +2,12 @@ onload = init();
 
 function init()
 {
-
+	//$("#BoxRegion").hide();
+	$("#RadialMenu").hide();
 }
 
-//Define a delegate to pass parameter to function
-//setInterval( function() { funca(10,3); }, 500 );
-
-function AnimateBoxRegion()
-{	
-	var elem = $("#BoxRegion");
-    AnimateHorizontal(elem, 10, -2000);
-}
-
-function AnimateHorizontal(elementObj, step, amount)
+function Hide()
 {
-	var id = setInterval(frame, 5);
-	
-	var o = GetXYCoords(elementObj);
-	var offset = 0;
-	
-	var direction = Math.sign(amount);
-	amount = Math.abs(amount);
-	
-	function frame()
-	{
-        offset = Math.min(offset + step, amount);		
-		SetXYCoords(elementObj, o.x + offset * direction, o.y);
-		if(offset == amount)
-			clearInterval(id);
-    }
-}
-
-function GetXYCoords(elementObj)
-{
-	var offset = elementObj.offset();
-	var width = elementObj.width() + elementObj.outerWidth();
-	var height = elementObj.height() + elementObj.outerHeight();
-
-	var centerX = offset.left + width / 2;
-	var centerY = offset.top + height / 2;
-	
-	return { x: centerX, y: centerY };
-}
-
-function SetXYCoords(elementObj, x, y)
-{
-	var width = elementObj.width() + elementObj.outerWidth();
-	var height = elementObj.height() + elementObj.outerHeight();
-		
-    elementObj.offset({left: x - (width / 2), top: y - (height / 2)});
+	$("#BoxRegion").hide();
+	$("#RadialMenu").show();
 }
