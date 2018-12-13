89c360e48a029fd714a32c3d2c3c551da748916a
Freak-ey <mkfrary@gmail.com>
2018-12-13 14:53:01 +0000

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -5,36 +5,25 @@ function init()
 	$("#BoxRegion").hide();
 	$("#RadialMenu").hide();
 	//$("#MusicMenu").hide();
-	
-	/*
-	$('.GridSquare').each(function() 
-	{
-		var value = GenerateRandomNumber(0, 3);
-		var url = "data/images/albums/";
 		
-		switch(value)
-		{
-			case 0:
-				url += "Agents of Fortune"
-				break;
-			case 1:
-				url += "Back in Black"
-				break;
-			case 2:
-				url += "Fire of Unknown Origin"
-				break;
-		}
-		url += ".jpg";
-		
-		$(this).css('background-image: url("' + url + '");'); 
-	});​
-	*/
-	
 	var numPerColumn = 6;
-	var num = 18;
+	var num = 30;
 	var scale = (100 / numPerColumn) - 1;
 	
-	var musicMenu = $("#MusicMenu");
+	$('.ScrollableField').each(function()
+	{
+		$(this).mousewheel(function(event, delta)
+		{
+			var offset = $(this).offset();
+			var distanceY = offset.top + delta * 20;
+	
+			$(this).offset({ top: distanceY, left: offset.left });
+			
+			console.log(distanceY);
+		});
+	});
+	
+	var musicMenu = $("#MusicMenu").find(".ScrollableField");
 	for(var i = 0; i < num; i++)
 		musicMenu.append("<div class='GridSquare'></div>");	
 	
@@ -62,7 +51,12 @@ function init()
 		
 		$(this).append("<div class='CardFront'> </div>");	
 		$(this).append("<div class='CardBack'> </div>"); //style='background-image: url(" + '"' + url + '"' + ")
-	});
+	});	
+}
+
+function OnHover()
+{	
+	console.log("Hover");
 }
 
 function Hide()
@@ -75,4 +69,4 @@ function Hide()
 function GenerateRandomNumber(min, max) 
 {
     return Math.floor(Math.random() * (max-min) + min);
-} 
+}
